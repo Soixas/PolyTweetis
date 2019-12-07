ada55c38cf8dd854403fd096f870e5f9dca6d1fb
Damien Rogé <damien.roge.info@gmail.com>
2019-12-07 08:28:02 +0100

    JUNIT pom & first test

---
+++ spring-data-keyvalue-examples-master/retwisj/src/main/java/org/springframework/data/redis/samples/retwisj/tests/MyFirstTest.java
@@ -0,0 +1,13 @@
+package org.springframework.data.redis.samples.retwisj.tests;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyFirstTest {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+
+}
